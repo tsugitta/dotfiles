c3e32bdeeee7516c65e8bf3ae9f88bc7c718d6f8
Toshinori Tsugita <tsugitta@gmail.com>
2016-05-07 04:51:59 +0900

    Fix bug that the program cannot skip .git etc

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ script_dir_path=$(dirname $(greadlink -f $0))
 
 for file_path in $script_dir_path/.??*
 do
-  file_name=${f##*/}
+  file_name=${file_path##*/}
   [[ $file_name == .git ]] && continue
   [[ $file_name == .DS_Store ]] && continue
 
