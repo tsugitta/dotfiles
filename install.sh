c303dfb468a0d0a26859910fe17029d1ac55359d
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-02-12 01:51:33 +0900

    No need to symlink `.gitignore` & `.gitmodules`

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ for file_path in $script_dir_path/.??*
 do
   file_name=${file_path##*/}
   [[ $file_name == .git ]] && continue
+  [[ $file_name == .gitignore ]] && continue
+  [[ $file_name == .gitmodules ]] && continue
   [[ $file_name == .DS_Store ]] && continue
 
   ln -sfv $file_path ~/
